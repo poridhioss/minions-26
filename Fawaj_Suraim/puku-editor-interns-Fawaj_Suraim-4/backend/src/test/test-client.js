a50baac1e68c4a52cbaad124d3ea20af475ae9d1
Fawaj Suraim <fawajsuraim@gmail.com>
2026-07-15 04:08:24 +0600

    Project 4 removed

--- Fawaj_Suraim/puku-editor-interns-Fawaj_Suraim-4/backend/src/test/test-client.js
+++
@@ -1,5 +0,0 @@
-// test-client.js
-const WebSocket = require('ws');
-const jobId = process.argv[2];
-const ws = new WebSocket(`ws://localhost:3000?jobId=${jobId}`);
-ws.on('message', (m) => console.log(m.toString()));
--- Fawaj_Suraim/puku-editor-interns-Fawaj_Suraim-4/backend/test/__bullmq_stub.js
+++
@@ -1,6 +0,0 @@
-class Queue {
-     constructor() {}
-     async getJob(id) { return global.__stubJobById?.[id] ?? (id === 'j1' ? global.__stubJob : undefined); }
-     async close() {}
-   }
-   module.exports = { Queue };
